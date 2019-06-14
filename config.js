1f4b648b28b4786dd951db2ddac3bbca99fe780e
comdet <listzone@hotmail.com>
2019-06-14 18:41:05 +0700

    state new version

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
     website : 'https://github.com/MakerAsia/arduino-esp32',
     git : 'https://github.com/MakerAsia/arduino-esp32',
     image : 'https://docs.espressif.com/projects/esp-idf/en/latest/_static/espressif-logo.svg', //'https://avatars1.githubusercontent.com/u/9460735',
-    version : '1.0.2',
+    version : '1.0.3',
     data : {
         name : 'arduino-esp32',
         platform : 'arduino-esp32',
